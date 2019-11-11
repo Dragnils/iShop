308c7b9710a746889b52714edfaf8664a62e1d5a
Anuarbek <anuarbekzhamanbaev@gmail.com>
2019-11-11 09:36:42 +0600

    Работа с cookie

--- src/main/java/net/devstudy/servlet/HelloWorldServlet.java
+++ src/main/java/net/devstudy/servlet/HelloWorldServlet.java
@@ -2,6 +2,7 @@ package net.devstudy.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,15 +14,22 @@ public class HelloWorldServlet extends HttpServlet {
 
 
 
-    @Override
+ /*   @Override
     public void init() throws ServletException {
         //TODO Auto-generated method stub
         super.init();
-    }
+    }*/
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
+
+        Cookie c = new Cookie("author", "devstudy");
+        c.setMaxAge(1800); // if negative,the cookie is not stored; if zero, deletes the cookie. время жизни куки в секундах
+        c.setPath("/"); // на какой url должны передаваться куки
+        c.setHttpOnly(true); // данный куки должен обрабатываться только на уровне протокола HTTP
+        resp.addCookie(c); // добавляем в responce куки
+
         PrintWriter out = resp.getWriter();
         out.println("<html><body>");
         out.println("Hello world!");
@@ -29,9 +37,9 @@ public class HelloWorldServlet extends HttpServlet {
         out.close();
     }
 
-    @Override
-    public void destroy() {
-        //TODO Auto-generated method stub
-        super.destroy();
-    }
+//    @Override
+//    public void destroy() {
+//        //TODO Auto-generated method stub
+//        super.destroy();
+//    }
 }
