c30f25abaa912d6aaa0c6f909f073618b60fd183
Anuarbek <anuarbekzhamanbaev@gmail.com>
2019-11-13 14:54:33 +0600

    Добавил константу ACCOUNT_ACTION_HISTORY для Listener

--- src/main/java/net/devstudy/Constants.java
+++ src/main/java/net/devstudy/Constants.java
@@ -5,6 +5,8 @@ public final class Constants {
     public static final int MAX_PRODUCT_COUNT_PER_SHOPPING_CART = 10; // максимальное количество одного и того же продукта на одну корзину
     public  static final int MAX_PRODUCTS_PER_SHOPPING_CART = 20; // максимальное количество продуктов в корзине
 
+    public static final String ACCOUNT_ACTIONS_HISTORY = "ACCOUNT_ACTIONS_HISTORY";
+
     public enum Cookie {
         SHOPPING_CART("iSCC", 60 * 60 * 24 * 365);
 
